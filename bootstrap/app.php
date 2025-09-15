ebe08d833f1255af2d3c8c40ebaaba36f3529651
shadowfer <fernely24k@gmail.com>
2025-09-15 17:19:36 -0600

    chore: admin route added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: __DIR__.'/../')
     ->withRouting(
@@ -10,6 +11,13 @@ return Application::configure(basePath: __DIR__.'/../')
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        //Añade Nueva Ruta
+        then: function(){
+            Route::middleware('web', 'auth')
+                ->prefix('admin')
+                ->name('admin.')
+                ->group(__DIR__.'/../routes/admin.php');
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
